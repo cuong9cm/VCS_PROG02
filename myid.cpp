feb2e3e554a3667a919c8a82bb07a4317a01c7f5
Manh Cuong <80744099+cuong9cm@users.noreply.github.com>
2022-09-24 21:17:48 +0700

    Update

--- myid.cpp
+++ myid.cpp
@@ -1,5 +1,6 @@
 #include<bits/stdc++.h>
 #include <pwd.h>
+#include <grp.h>
 #include <unistd.h>
 using namespace std;
 
@@ -8,12 +9,16 @@ int main() {
     cout << "Nhap ten user can tim: ";
     cin >> user;
     struct passwd *usr;
+    struct group *gr;
     usr = getpwnam(user.c_str());
+    gr = getgrnam(user.c_str());
     if (!usr) {
         cout << "Khong tim thay user" << endl;
         return 0;    
     }
     cout << "uid: " << usr->pw_uid;
-    cout << " gid: " << usr->pw_gid;
+    cout << "\ngid: " << usr->pw_gid;
+    cout << "\ndir home: " << usr->pw_dir;
+    cout << "\ngroup name: " << gr->gr_name;
     return 0;
 }
