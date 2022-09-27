d625a558fd2e514a1a58939e72f9557563599496
Manh Cuong <80744099+cuong9cm@users.noreply.github.com>
2022-09-27 00:36:31 -0400

    Add files via upload

---
+++ mypassword.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#include <shadow.h>
+#include <pwd.h>
+#include <crypt.h>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+	cout << "Enter username: ";
+	string username;
+	cin >> username;
+	struct spwd *userInfo = getspnam(username.c_str());
+	cout << "Changing password for " << username << endl;
+
+	string currentPassword;
+	cout << "Enter current password: ";
+	cin >> currentPassword;
+
+	string encrypt_password = crypt(currentPassword.c_str(), userInfo->sp_pwdp);
+	if (strcmp(encrypt_password.c_str(), userInfo->sp_pwdp) != 0) {
+		cout << "Wrong passworld!" << endl;
+		return 0;
+	}
+	string newPassword;
+	cout << "Enter new password: ";
+	cin >> newPassword;
+
+	encrypt_password = crypt(newPassword.c_str(), userInfo->sp_pwdp);
+	userInfo->sp_pwdp = encrypt_password.c_str();
+
+	FILE *fileDest = fopen("/etc/shadow", "r");
+	FILE *fileTemp = fopen("/tmp/shadow.tmp", "w");
+
+	char* line;
+	size_t len = 0;
+	while (getline(&line, &len, fileDest) != -1) {
+		if (strstr(line, username.c_str()) != NULL) {
+			putspent(userInfo, fileTemp);
+		}
+		else {
+			fputs(line, fileTemp);
+		}
+	}
+
+	cout << "\nSuccessfully";
+	remove("/etc/shadow");
+	rename("/tmp/shadow.tmp", "/etc/shadow");
+
+	fclose(fileDest);
+	fclose(fileTemp);
+	return 0;
+}
